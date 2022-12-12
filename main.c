cae197465b454d8a2d37892e65fac000a8eb349b
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-12 15:53:36 +0300

    упражнение 1.5

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
 
     printf("Таблица температур:\n");
 
-    for (fahr = 0; fahr <= 300; fahr += 20)
+    for (fahr = 300; fahr >= 0; fahr -= 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 
     return 0;
