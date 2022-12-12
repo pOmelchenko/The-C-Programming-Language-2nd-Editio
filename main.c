68d434364fce843c4d98a16f2157f9775113b10f
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-12 15:40:51 +0300

    упражнение 1.4

--- main.c
+++ main.c
@@ -4,18 +4,18 @@ int main() {
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;
+    lower = -50;
     upper = 300;
-    step = 20;
+    step = 10;
 
-    fahr = lower;
+    celsius = lower;
 
     printf("Таблица температур:\n");
 
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+    while (celsius <= upper) {
+        fahr = (celsius * 9.0/5.0) + 32.0;
+        printf("%3.1f %6f\n", celsius, fahr);
+        celsius += step;
     }
 
     return 0;
