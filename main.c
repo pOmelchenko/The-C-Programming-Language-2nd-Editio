8b2724a7647722f2ca79af6e55818b1a93bd7ffc
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-12 22:59:01 +0300

    упражнение 1.8

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int character, spaces;
+    int character, new_line, tabs, spaces;
 
+    new_line = 0;
+    tabs = 0;
     spaces = 0;
     while ((character = getchar()) != EOF) {
+        if ('\n' == character) {
+            ++new_line;
+        }
+        if ('\t' == character) {
+            ++tabs;
+        }
         if (' ' == character) {
             ++spaces;
-        } else {
-            spaces = 0;
         }
-
-        if (spaces > 1) {
-            continue;
-        }
-
-        putchar(character);
     }
+
+    printf("lines: %d\ntabs: %d\nspaces: %d\n", new_line, tabs, spaces);
 
     return 0;
 }
