88fae0232a113085927f28a3a9c39ee3014c76c5
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-12 15:50:24 +0300

    глава 1.3

--- main.c
+++ main.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    float fahr, celsius;
-    int lower, upper, step;
-
-    lower = -50;
-    upper = 300;
-    step = 10;
-
-    celsius = lower;
+    int fahr;
 
     printf("Таблица температур:\n");
 
-    while (celsius <= upper) {
-        fahr = (celsius * 9.0/5.0) + 32.0;
-        printf("%3.1f %6f\n", celsius, fahr);
-        celsius += step;
-    }
+    for (fahr = 0; fahr <= 300; fahr += 20)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 
     return 0;
 }
