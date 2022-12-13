6379c4f3843438f6444765da6cbb9786e3cf55af
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-13 15:08:12 +0300

    глава 1.6

--- main.c
+++ main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 int main() {
-    int character, state;
+    int character, index, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for (index = 0; index < 10; ++index)
+        ndigit[index] = 0;
 
     while ((character = getchar()) != EOF) {
-        if (' ' == character || '\n' == character || '\t' == character) {
-            if (OUT == state) {
-                continue;
-            } else {
-                putchar('\n');
-            }
-            state = OUT;
-        } else {
-            state = IN;
-            putchar(character);
-        }
+        if (character >= '0' && character <= '9')
+            ++ndigit[character - '0'];
+        else if (character == ' ' || character == '\n' || character == '\t')
+            ++nwhite;
+        else
+            ++nother;
     }
+
+    for (index = 0; index < 10; ++index)
+        printf(" %d", ndigit[index]);
+    printf(", white_space = %d, other = %d\n", nwhite, nother);
 
     return 0;
 }
