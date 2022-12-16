599158ed3dbad3084843ef3ae804c4e6a21c8435
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-17 00:28:20 +0300

    упражнение 1.13

--- main.c
+++ main.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
+/**
+ * \brief Упражнение 1.13
+ *
+ * \details Напишите программу для вывода гистограммы длин слов во входном потоке.
+ * Построить гистограмму с горизонтальными рядами легко, а вот с вертикальными столбцами трудней.
+ *
+ * \return int
+ */
 int main() {
-    int character, index, nwhite, nother;
-    int ndigit[10];
+    int max_length = 10, length = 0, character, state;
 
-    nwhite = nother = 0;
-    for (index = 0; index < 10; ++index)
-        ndigit[index] = 0;
+    // инициализировать массив указав лимитом количество символов в самом длинном слове
+    int words[max_length];
 
-    while ((character = getchar()) != EOF) {
-        if (character >= '0' && character <= '9')
-            ++ndigit[character - '0'];
-        else if (character == ' ' || character == '\n' || character == '\t')
-            ++nwhite;
-        else
-            ++nother;
+    // обнулить все элементы массива
+    for (int i = 0; i < max_length; i++) {
+        words[i] = 0;
     }
 
-    for (index = 0; index < 10; ++index)
-        printf(" %d", ndigit[index]);
-    printf(", white_space = %d, other = %d\n", nwhite, nother);
+    // пересчитать все слова
+    while ((character = getchar()) != EOF) {
+        if (' ' == character || '\n' == character || '\t' == character) {
+            if (OUT == state) {
+                continue;
+            }
+
+            ++words[length];
+
+            state = OUT;
+            length = 0;
+        } else {
+            state = IN;
+            ++length;
+        }
+    }
+
+    // вывести гистограмму
+    for (int word = 0; word < max_length; word++) {
+        printf("%3d | %4d | ", word, words[word]);
+        for (int row = 0; row < words[word]; row++) printf("#");
+        printf("\n");
+    }
 
     return 0;
 }
