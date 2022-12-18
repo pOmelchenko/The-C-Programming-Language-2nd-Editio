6580f3197331167827a48a85334d7695d08c446a
Pavel Omelchenko <p.Omelchenko@gmail.com>
2022-12-18 23:04:56 +0300

    упражнение 1.17

--- main.c
+++ main.c
@@ -1,73 +1,61 @@
 #include <stdio.h>
 
 /**
- * \brief Максимальная длина строки в потоке
+ * \brief Максимальный размер кэша
  */
-#define MAXLINE 1000
+#define MAX_LINE_CACHE 80
 
 /**
  * \brief Символ конца строки
  */
 #define EOL '\0'
 
-int get_line(char line[], int maxline);
-
 /**
- * \brief Упражнение 1.16
+ * \brief Упражнение 1.17
  *
- * \details Доработайте главный модуль программы определения самой длинной строки так, чтобы она выводила правильное
- * значение для какой угодно длины строк входного потока, насколько это позволяет текст
+ * \details Напишите программу для вывода всех строку входного потока имеющих длину более 80 символов.
  *
- * \details Revise the main routine of the longest-line program so it will correctly print the length of arbitrary
- * long input lines, and as much as possible of the text.
+ * \details Write a program to print all input lines that are longer than 80 characters.
  *
  * \return код завершения программы
  */
 int main() {
     /**
-     * \brief Длина текущей строки
+     * \brief Символ из потока
      */
-    int len;
+    int character;
 
     /**
-     * \brief Текущая введенная строка
+     * \brief Количество символов в строке
      */
-    char line[MAXLINE];
+    int character_number = 0;
 
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        printf("%4d | %s", len, line);
-    }
+    /**
+     * \brief Кэш строки
+     */
+    char line_cache[MAX_LINE_CACHE];
 
-    return 0;
-}
+    while ((character = getchar()) != EOF) {
+        if (character_number < MAX_LINE_CACHE - 1 && character != '\n') {
+            line_cache[character_number++] = character;
+            continue;
+        }
 
-/**
- * \brief get_line
- *
- * \details считывает строку в s, возвращает ее длину
- *
- * \param s строка которую читаем
- * \param lim лимит символов в строке
- * \return int длина строки
- */
-int get_line(char s[], int lim) {
-    int c, i;
+        if (character_number == MAX_LINE_CACHE - 1 && character != '\n') {
+            line_cache[character_number++] = EOL;
+            printf("%s", line_cache);
+        }
 
-    i = 0;
-
-    while ((c = getchar()) != EOF && c != '\n') {
-        if (i < lim - 1) {
-            s[i++] = c;
+        if (character != '\n') {
+            putchar(character);
+            character_number++;
         } else {
-            i++;
+            if (character_number > MAX_LINE_CACHE) {
+                putchar(character);
+            }
+            character_number = 0;
         }
     }
 
-    if (c == '\n') {
-        s[i++] = c;
-    }
-
-    s[i] = EOL;
-
-    return i;
+    return 0;
 }
